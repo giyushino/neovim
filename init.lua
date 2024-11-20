4d0302f387ff7922fd4fda5a0450b5486b5610c6
allan <allanzhang440@gmail.com>
2024-11-20 00:51:10 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -17,6 +17,12 @@ require("lazy").setup({
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },  -- Telescope for fuzzy finding
   { 'folke/neodev.nvim', config = function() require('neodev').setup() end },  -- Neodev for Lua LSP
   {
+    'andweeb/presence.nvim',
+    config = function()
+      require('presence').setup()
+    end
+  },
+  {
     "nvim-tree/nvim-tree.lua",
     config = function()
       require("nvim-tree").setup({
@@ -28,11 +34,11 @@ require("lazy").setup({
   { 'lervag/vimtex', lazy = false, init = function()
     vim.g.vimtex_view_method = "general"
     vim.g.vimtex_view_pdf_method = "start"
+    --vim.g.vimtex_view_sioyek_inverse_search = "C:\\Program Files\\Neovim\\bin\\nvim.exe --headless -c \"VimtexInverseSearch %2 '%1'\""
     vim.g.vimtex_view_forward_search = 1
-    vim.g.vimtex_view_sumatra_executable = "C:/Users/allan/Downloads/SumatraPDF-3.5.2-64/SumatraPDF-3.5.2-64.exe"
+    vim.g.vimtex_view_sumatra_executable = "C:/Users/allan/AppData/Local/SumatraPDF/SumatraPDF.exe"
   end },
 })
-
 vim.opt.number = true
 -- Set colorscheme after plugins are installed
 vim.cmd('colorscheme gruvbox')
@@ -48,14 +54,14 @@ require("snacks").setup({
     autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
     preset = {
       keys = {
-        { icon = "🧌 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-        { icon = "🧌 ", key = "n", desc = "New File", action = ":ene | startinsert" },
-        { icon = "🧌 ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-        { icon = "🧌 ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-        { icon = "🧌 ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-        { icon = "🧌 " , key = "s", desc = "Restore Session", section = "session" },
-        { icon = "🧌 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-        { icon = "🧌 ", key = "q", desc = "Quit", action = ":qa" },
+	{ icon = "🧌 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = "🧌 ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = "🧌 ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = "🧌 ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      { icon = "🧌 ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = "🧌 ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = "🧌 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = "🧌 ", key = "q", desc = "Quit", action = ":qa" },
       },
       header = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
@@ -200,4 +206,27 @@ vim.cmd([[
   highlight CmpItemKindVariable guifg=#F92672 gui=bold
 ]])
 
+-- The setup config table shows all available config options with their default values:
+require("presence").setup({
+    -- General options
+    auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+    neovim_image_text   = "too much fucking work for this shit", -- Text displayed when hovered over the Neovim image
+    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+   -- client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    enable_line_number  = false,                      -- Displays the current line number instead of the current project
+    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time           = true,                       -- Show the timer
 
+    -- Rich Presence text options
+    editing_text        = "I FUCKING HATE EDITING %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+})
