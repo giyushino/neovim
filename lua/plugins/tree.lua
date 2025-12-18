dae25f24236237a359329d9e72b9a99d89b0ef03
giyushino <allanzhang440@gmail.com>
2025-12-17 23:44:23 -0800

    update tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-tree/nvim-tree.lua",
+  lazy = false,
   version = "*",
   dependencies = {
     "nvim-tree/nvim-web-devicons", -- optional, for file icons
@@ -26,6 +27,7 @@ return {
 
     -- Toggle file tree
     vim.keymap.set("n", "<leader>m", ":NvimTreeToggle<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>tf", ":NvimTreeFindFile<CR>", { desc = "NvimTree Find File" })
   end,
 }
 
